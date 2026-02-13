74bd68babb0113f579b43aafc176e3bc2fa14f7a
George Peydy <greenstarmain2gmail.com>
2026-02-13 18:43:05 +0200

    -feat: disable scrolling, when .menu is active

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,28 @@
-import '../styles/base/_styles.scss';
 'use strict';
+
+import '../styles/base/_styles.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.header__menu-button');
+
+  if (menuBtn) {
+    menuBtn.onclick = () => {
+      document.body.classList.toggle('is-locked');
+      document.querySelector('.menu').classList.toggle('is-active');
+    };
+  }
+
+  const closeElements = document.querySelectorAll('.menu__link, .menu__close');
+
+  closeElements.forEach(link => {
+    link.addEventListener('click', () => {
+      document.body.classList.remove('is-locked');
+
+      const menu = document.querySelector('.menu');
+
+      if (menu) {
+        menu.classList.remove('is-active');
+      }
+    });
+  });
+});
